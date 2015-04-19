ceb76808723b928b50a329830797ad9a430dc467
harikishen <hari751995@gmail.com>
2015-04-19 09:32:39 +0530

    Modified

--- date.js
+++ date.js
@@ -1,10 +1,6 @@
 $(function() {
 	  $("input[type='date']").datepicker({
-<<<<<<< HEAD
-     dateFormat: "yy-mm-dd",yearRange: "-100:+0",changeMonth: true,changeYear: true,defaultDate:"2000-01-01"
-=======
      dateFormat: "yy-mm-dd",changeMonth: true,changeYear: true,yearRange:"1950:2050"
->>>>>>> Date Modified
     });
     $("input[type='date']").datepicker();
   });
